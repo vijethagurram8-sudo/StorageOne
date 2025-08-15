172c2816fb896aa5283c2a33716ce54e97b973f0
SANDHIYA <SANDHIYA@DESKTOP-IRHS2JI>
2025-08-15 22:36:41 +0530

    sasssss

--- sai/src/test/java/classtest/SeleniumPractise.java
+++ sai/src/test/java/classtest/SeleniumPractise.java
@@ -4,7 +4,7 @@ public class SeleniumPractise {
 
 	public static void main(String[] args) {
 		System.out.println("succusfully -------");
-
+System.err.println("fyuuyhhulagga");
 	}
 
 }
--- sai/src/test/java/classtest2/SeleniumPracticeTwo.java
+++ sai/src/test/java/classtest2/SeleniumPracticeTwo.java
@@ -4,7 +4,7 @@ public class SeleniumPracticeTwo {
 
 	public static void main(String[] args) {
 		System.out.println(" ----two succusfully-----");
-
+System.out.println("hyhsiojijixshhcujicopl");
 	}
 
 }
